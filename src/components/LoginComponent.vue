2e60d56af0e7eac4166c2f591e58380210d2a4b2
oskar2115 <oskarpiotrkraw@gmail.com>
2023-11-03 13:26:05 +0100

    f3

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -16,6 +16,29 @@
 </template>
 
 <style scoped>
+input {
+	transition: 180ms ease-in-out;
+	background-color: rgb(222, 222, 222);
+	width: 100%;
+	padding: 12px 20px;
+	margin: 8px 0;
+	box-sizing: border-box;
+	border-radius: 15px;
+	border: 3px solid rgb(0, 0, 0);
+	box-shadow: -5px 5px 27px -2px rgba(0, 0, 0, 0.5)
+}
+
+input:focus {
+	background-color: rgb(255, 255, 255);
+	border: 5px solid rgb(52, 52, 168);
+	outline: 3px solid transparent;
+}
+
+input[type="submit"] {
+	background-color: rgb(255, 255, 255);
+	width: 60%;
+}
+
 label {
 	font-size: 1.25em;
 }
@@ -23,15 +46,16 @@ label {
 form {
 	display: flex;
 	width: 80%;
-	height: 70%;
+	height: 100%;
 	align-items: center;
 	flex-direction: column;
-	margin-bottom: 50%;
+	margin-top: 5%;
 }
 
 #Login {
-	margin-bottom: 10%;
-	font-size: 3em;
+	position: absolute;
+	margin-bottom: 120%;
+	font-size: 5em;
 }
 
 .LoginMain {
@@ -40,8 +64,8 @@ form {
 	justify-content: center;
 	align-items: center;
 	background-color: var(--color-background-soft);
-	height: 50%;
-	width: 22%;
+	height: 25%;
+	width: 18%;
 	position: absolute;
 	border-radius: 2%;
 	box-shadow: -15px 16px 11px -4px rgba(12, 12, 12, 0.5);
